29adcf6533fcf890f50bd5dfdaff4018994fbb2e
Miguel Molina <hi@mvader.me>
2016-02-03 21:10:26 +0100

    Initial Commit

---
+++ Subtle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Subtle
+//
+//  Created by Miguel Molina on 03/02/16.
+//  Copyright © 2016 Miguel Molina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
